ac63c4620afa748affe3c041ebc8be5202eb42fe
Lucas Jundi Hikazudani <lhjundi@proton.me>
2024-10-18 12:50:04 -0300

    Initial commit

---
+++ src/test/java/com/lhjundi/EnergyMonitoringApplication/EnergyMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhjundi.EnergyMonitoringApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
